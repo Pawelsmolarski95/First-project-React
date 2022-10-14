2acab394308c03384529bf4ee40696a5a4dce603
Pawelsmolarski95 <109138583+Pawelsmolarski95@users.noreply.github.com>
2022-10-14 23:27:17 +0200

    problem reference addCard

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,10 +1,20 @@
 import styles from './Column.module.scss';
 import 'font-awesome/css/font-awesome.min.css';
+import Card from '../Card/Card'
+import CardForm from '../CardForm/CardForm';
+
 
 const Column = (props) => {
+    
     return (
         <article className={styles.column}>
             <h2 className={styles.title}><span className={styles.icon + " fa fa-" + props.icon}></span> {props.title} </h2>
+            <ul className='styles.cards'>
+                {props.cards.map(card => 
+                <Card key={card.id } title={card.title} />)}
+            </ul>
+            <CardForm columnId={props.id} action={props.addCard}/>
+           
         </article>
     )
 }
